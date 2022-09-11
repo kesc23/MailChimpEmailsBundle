a1fb2ff92e96a8ba80e0028311fb31a8c5569e59
Kevin Campos <kesc23@hotmail.com>
2022-09-11 15:18:53 -0300

    Commited inital config and main plugin files

---
+++ Config/config.php
@@ -0,0 +1,14 @@
+<?php 
+return array(
+    'name'        => 'MailChimp Emails',
+    'description' => 'Create/Rescue your emails in MailChimp and bring\'em to Mautic',
+    'author'      => 'Kesc23 (Kevin Campos)',
+    'version'     => '1.0.0',
+    'routes'      => [
+        'main'    => [],
+    ],
+    'menu'        => [
+        'main'    => [],
+        'admin'   => [],
+    ],
+);
---
+++ MailChimpEmailsBundle.php
@@ -0,0 +1,5 @@
+<?php
+namespace MauticPlugin\MailChimpEmailsBundle;
+use Mautic\PluginBundle\Bundle\PluginBundleBase;
+
+class MailChimpEmailsBundle extends PluginBundleBase{}
