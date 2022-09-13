bb9894a011426f55194b73e9aaea66c1f82ee795
Kevin Campos <kesc23@hotmail.com>
2022-09-13 09:01:33 -0300

    Update DefaultController.php

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -17,14 +17,12 @@ class DefaultController extends CommonController{
 
         $client = HttpClient::create([
             'headers' => [
-                'Authorization' => 'Bearer 5d1d400be387648f4c8fa28ea3c7cc52-us1',
                 'Content-Type'  => 'application/json'
             ],
         ]);
 
         $response = $client->request(
             'GET',
-            'https://us1.api.mailchimp.com/3.0/campaigns/81432f3aad/content?fields=html'
         );
 
         return $this->delegateView(
